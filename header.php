98e710bc8e27a6852611b44da587951b5fd2d6b0
Vincent van 't Westende <vincent@zimmermanzimmerman.nl>
2015-01-20 12:40:00 +0100

    remove description meta

--- header.php
+++ header.php
@@ -4,7 +4,6 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="">
 <meta name="author" content="">
 <link rel="icon" type="image/x-icon" href="<?php echo get_stylesheet_directory_uri(); ?>>/favicon.ico" />
 <link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png" />
